2049cbb1b88635f0bd71907c8a255ec3a396d4b2
conceitusnet <conceitusnet@yahoo.com>
2017-01-24 21:43:43 -0300

    Adicionado um full_title ao helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Retorna o título completo por página
+  def full_title(page_title = '')
+    base_title = "BetsBolão sites de apostas do brasileirão"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  	def setup
-  		@base_title = "BetsBolão sites de apostas do brasileirão"
-  	end	
+  	#def setup
+  	#	@base_title = "BetsBolão sites de apostas do brasileirão"
+  	#end
 
   #test "should get root" do
    # get FILL_IN
@@ -14,19 +14,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "BetsBolão sites de apostas do brasileirão"
   end
 
   test "should get ajuda" do
     get static_pages_ajuda_url
     assert_response :success
-    assert_select "title", "Ajuda | #{@base_title}"
+    assert_select "title", "Ajuda | BetsBolão sites de apostas do brasileirão"
   end
 
   test "should get sobre" do
   	get static_pages_sobre_url
   	assert_response :success
-  	assert_select "title", "Sobre | #{@base_title}"
+  	assert_select "title", "Sobre | BetsBolão sites de apostas do brasileirão"
   end
 
 end
